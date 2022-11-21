ead798c12be0ce3e55604d369ea872ece42351df
Raihan Mahamud <48452737+emonrait@users.noreply.github.com>
2022-11-21 13:03:31 +0600

    Ui Design update

---
+++ app/src/main/java/com/emon/raihan/dynamicutility/view/loan/LoanDashboard.kt
@@ -0,0 +1,14 @@
+package com.emon.raihan.dynamicutility.view.loan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.emon.raihan.dynamicutility.R
+
+class LoanDashboard : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loan_dashboard)
+
+
+    }
+}
