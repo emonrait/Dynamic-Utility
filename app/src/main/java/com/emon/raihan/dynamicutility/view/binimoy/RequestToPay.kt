f4697adc2451a6692729b154a8ace6dd07261400
Raihan Mahamud <48452737+emonrait@users.noreply.github.com>
2022-11-24 13:44:21 +0600

    Update binimoy Intregation

--- app/src/main/java/com/emon/raihan/dynamicutility/view/binimoy/RequestToPay.kt
+++ app/src/main/java/com/emon/raihan/dynamicutility/view/binimoy/RequestToPay.kt
@@ -1,12 +1,35 @@
 package com.emon.raihan.dynamicutility.view.binimoy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
 import com.emon.raihan.dynamicutility.R
+import com.emon.raihan.dynamicutility.util.CustomActivityClear
 
 class RequestToPay : AppCompatActivity() {
+    private lateinit var toolbar: Toolbar
+    private lateinit var iv_header_back: ImageView
+    private lateinit var toolbar_title: TextView
+    private lateinit var iv_header_logout: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_request_to_pay)
+
+        toolbar = findViewById(R.id.toolbar)
+        iv_header_back = toolbar.findViewById(R.id.iv_header_back)
+        toolbar_title = toolbar.findViewById(R.id.toolbar_title)
+        iv_header_logout = toolbar.findViewById(R.id.iv_header_logout)
+
+        setSupportActionBar(toolbar)
+        toolbar_title.text = "Request To Pay"
+
+
+        iv_header_back.setOnClickListener {
+            val intent = Intent(this, BinimoyDashboard::class.java)
+            CustomActivityClear.doClearActivity(intent, this)
+        }
     }
 }
