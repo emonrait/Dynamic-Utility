90b3331c217608154ec6115bdd6278541653ae36
Raihan Mahamud <48452737+emonrait@users.noreply.github.com>
2022-11-28 09:47:17 +0600

    Update Dialog

--- app/src/main/java/com/emon/raihan/dynamicutility/view/ElectricityBillPayment.kt
+++ app/src/main/java/com/emon/raihan/dynamicutility/view/ElectricityBillPayment.kt
@@ -397,7 +397,13 @@ class ElectricityBillPayment : CustomAppCompatActivity() {
         dropdown_recycler.adapter = mAdapter
         mAdapter?.notifyDataSetChanged()
 
-
+        et_bill_type_value.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable) {
+                mAdapter.filter.filter(s)
+            }
+        })
         ivClose.setOnClickListener { alertDialog.dismiss() }
 
 
