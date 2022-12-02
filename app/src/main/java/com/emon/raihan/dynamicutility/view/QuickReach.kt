9c285f7b2ea566e0d3d74e709c3ef496decd342d
emonrait <emonrait@gmail.com>
2022-12-03 04:48:13 +0600

    update ui design

--- app/src/main/java/com/emon/raihan/dynamicutility/view/QuickReach.kt
+++ app/src/main/java/com/emon/raihan/dynamicutility/view/QuickReach.kt
@@ -1,6 +1,10 @@
 package com.emon.raihan.dynamicutility.view
 
 import android.os.Bundle
+import com.denzcoskun.imageslider.ImageSlider
+import com.denzcoskun.imageslider.constants.ScaleTypes
+import com.denzcoskun.imageslider.models.SlideModel
+import com.emon.raihan.dynamicutility.R
 
 import com.emon.raihan.dynamicutility.util.CustomAppCompatActivity
 
@@ -10,9 +14,18 @@ class QuickReach : CustomAppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(com.emon.raihan.dynamicutility.R.layout.activity_quick_reach)
+        setContentView(R.layout.activity_quick_reach)
 
 
+        val imageSlider = findViewById<ImageSlider>(R.id.imageSlider)
+        val imageList = ArrayList<SlideModel>()
+
+        imageList.add(SlideModel("https://images.immediate.co.uk/production/volatile/sites/3/2019/04/Avengers-Endgame-Banner-2-de7cf60.jpg?quality=90&resize=620,413","Avengers Endgame"))
+        imageList.add(SlideModel("https://img.cinemablend.com/filter:scale/quill/3/7/0/0/8/e/37008e36e98cd75101cf1347396eac8534871a19.jpg?mw=600","Jumanji"))
+        imageList.add(SlideModel("https://www.adgully.com/img/800/201711/spider-man-homecoming-banner.jpg","Spider Man"))
+        imageList.add(SlideModel("https://live.staticflickr.com/1980/29996141587_7886795726_b.jpg","Venom"))
+
+        imageSlider.setImageList(imageList, ScaleTypes.FIT)
 
     }
 
