a5ad131947d463784bd3cba748b2946f64eff90d
emonrait <emonrait@gmail.com>
2022-12-03 02:46:10 +0600

    update ui design

--- app/src/main/java/com/emon/raihan/dynamicutility/view/welcome/fragment/Menu.kt
+++ app/src/main/java/com/emon/raihan/dynamicutility/view/welcome/fragment/Menu.kt
@@ -5,16 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
 import com.emon.raihan.dynamicutility.R
 
 class Menu : Fragment() {
+    private lateinit var toolbar: Toolbar
+    private lateinit var iv_header_back: ImageView
+    private lateinit var toolbar_title: TextView
+    private lateinit var iv_header_logout: ImageView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu, container, false)
+        val view = inflater.inflate(R.layout.fragment_menu, container, false)
+        toolbar = view.findViewById(R.id.toolbar)
+        iv_header_back = toolbar.findViewById(R.id.iv_header_back)
+        toolbar_title = toolbar.findViewById(R.id.toolbar_title)
+        iv_header_logout = toolbar.findViewById(R.id.iv_header_logout)
+
+        toolbar_title.text = getString(R.string.raihan_mahamud_emon)
+
+        return view
     }
 
 }
